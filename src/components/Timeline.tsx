e08d545d2975e3591c13d45b469251c7a13bdcbf
ishanghimire11 <ishanghimire11@gmail.com>
2024-03-27 23:30:53 +0545

    fix: timeline content

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -5,7 +5,7 @@ const Timeline = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="pb-16">Education and Experience</h1>
-      <TimelineContent />
+      {/* <TimelineContent /> */}
     </div>
   );
 };
@@ -34,32 +34,32 @@ const TimelineDotsandLines = ({
   );
 };
 
-export const TimelineContent = () => {
-  return (
-    <div>
-      {contents.map((content, index) => {
-        const { title, subtitle, date, isLast, isFirst } = content;
-        const isEven = index % 2 === 0;
+// export const TimelineContent = () => {
+//   return (
+//     <div>
+//       {contents.map((content, index) => {
+//         const { title, subtitle, date, isLast, isFirst } = content;
+//         const isEven = index % 2 === 0;
 
-        return (
-          <div
-            key={index}
-            className={`flex content-start ${
-              isEven ? "flex" : "flex-row-reverse text-right"
-            }`}
-          >
-            <div className="w-80 px-8 mb-28">
-              <h3 className="text-3xl">{title}</h3>
-              <p>{subtitle}</p>
-              <p>{date}</p>
-            </div>
-            {isFirst && <TimelineDotsandLines isFirst={true} />}
-            {!isFirst && !isLast && <TimelineDotsandLines />}
-            {isLast && <TimelineDotsandLines isLast={true} />}
-            <div className="w-80 px-8 mb-28"></div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+//         return (
+//           <div
+//             key={index}
+//             className={`flex content-start ${
+//               isEven ? "flex" : "flex-row-reverse text-right"
+//             }`}
+//           >
+//             <div className={`w-80 px-8 mb-28 ${}`}>
+//               <h3 className="text-3xl">{title}</h3>
+//               <p>{subtitle}</p>
+//               <p>{date}</p>
+//             </div>
+//             {isFirst && <TimelineDotsandLines isFirst={true} />}
+//             {!isFirst && !isLast && <TimelineDotsandLines />}
+//             {isLast && <TimelineDotsandLines isLast={true} />}
+//             <div className="w-80 px-8 mb-28"></div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
