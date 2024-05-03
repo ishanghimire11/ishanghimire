f3fe79ea4991a374db95ac3b8ac4c5bfb789954e
ishanghimire11 <ishanghimire11@gmail.com>
2024-05-03 23:52:27 +0545

    fix: small padding issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import HeroImage from "@/assets/hero.webp";
 
 const Hero = () => {
   return (
-    <section className="py-12 md:py-32 2xl:pt-20 2xl:pb-80">
+    <section className="py-12 md:py-32 2xl:pt-20">
       <div className="flex flex-col md:flex-row md:justify-between gap-x-24">
         <div className="max-w-[80em] w-fit flex flex-col justify-center pb-8 md:pb-0">
           <h1 className="text-6xl font-medium sm:text-7xl md:text-8xl pb-4 sm:pb-6 lg:pb-12 lg:text-9xl 2xl:text-[10rem] max-w-[50rem]">
